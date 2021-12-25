cc7ef95143da86b46c3da5761ca97585205d97b7
Emery Bashige <bemeryb@gmail.com>
2021-12-25 16:12:03 +0300

    created db dao methods

---
+++ app/src/main/java/com/example/attendanceapp/data/local/entity/AttendanceAppDao.kt
@@ -0,0 +1,36 @@
+package com.example.attendanceapp.data.local.entity
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.attendanceapp.domain.models.Attendance
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface AttendanceAppDao {
+
+    @Insert
+    fun insertAttendee(attendee: AttendeeEntity)
+
+    @Insert
+    fun insertEvent(event: EventEntity)
+
+    @Insert
+    fun insertAttendance(attendees: List<AttendanceEntity> )
+
+    @Query("SELECT * FROM AttendanceEntity WHERE eventId = :eventId ORDER BY day DESC")
+    fun getAttendanceByEvent(eventId: Int): Flow<List<Attendance>>
+
+    @Query("SELECT * FROM AttendanceEntity WHERE attendeeId = :attendeeId AND eventId = :eventId ORDER BY day DESC")
+    fun getAttendanceByAttendee(attendeeId: Int, eventId: Int): Flow<List<Attendance>>
+
+    @Query("SELECT * FROM AttendanceEntity WHERE day = :day AND eventId = :eventId ORDER BY time DESC")
+    fun getAttendanceByDay(day: String, eventId: Int): Flow<List<Attendance>>
+
+    @Query("SELECT * FROM AttendeeEntity WHERE eventDbId = :eventId ORDER BY personDbId DESC")
+    fun getAttendeeByEvent(eventId: Int): Flow<List<AttendeeEntity>>
+
+    @Query("SELECT * FROM EventEntity ORDER BY eventId DESC")
+    fun getEvents(): Flow<List<EventEntity>>
+}
