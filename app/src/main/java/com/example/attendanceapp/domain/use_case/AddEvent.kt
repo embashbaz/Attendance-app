7108953b81379356e62ff554477bea54f2da8bd2
Emery Bashige <bemeryb@gmail.com>
2021-12-31 13:49:15 +0300

    added use case method to add new event

---
+++ app/src/main/java/com/example/attendanceapp/domain/use_case/AddEvent.kt
@@ -0,0 +1,21 @@
+package com.example.attendanceapp.domain.use_case
+
+import com.example.attendanceapp.core.utils.OperationStatus
+import com.example.attendanceapp.domain.models.Event
+import com.example.attendanceapp.domain.repository.AttendanceMainRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class AddEvent(private val repository: AttendanceMainRepository)
+{
+    suspend operator fun invoke(eventType: String, eventName: String): Flow<OperationStatus<String>> {
+        if (eventType.isBlank() || eventName.isBlank()){
+            return flow {
+                emit(OperationStatus.Error<String>(message = "Event type and event name can not be blank"))
+            }
+        }
+        val newEvent = Event(0, eventType, eventName, "")
+        return repository.insertEvent(newEvent)
+    }
+
+}
