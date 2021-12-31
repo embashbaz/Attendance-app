7a707e2ef1d15ba8fc31047673a02955e89cd833
Emery Bashige <bemeryb@gmail.com>
2021-12-31 18:18:37 +0300

    added use case method to get all participants

---
+++ app/src/main/java/com/example/attendanceapp/domain/use_case/GetAllEventParticipant.kt
@@ -0,0 +1,24 @@
+package com.example.attendanceapp.domain.use_case
+
+import com.example.attendanceapp.core.utils.OperationStatus
+import com.example.attendanceapp.domain.models.Attendee
+import com.example.attendanceapp.domain.repository.AttendanceMainRepository
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.Flow
+
+class GetAllEventParticipant(private val repository: AttendanceMainRepository) {
+
+    suspend operator fun invoke(eventId: Int): Flow<OperationStatus<List<Attendee>>>{
+      if (eventId < 0){
+
+          return flow {
+              emit(OperationStatus.Error(message = "Invalid event id"))
+
+          }
+      }
+
+       return repository.getAllParticipants(eventId)
+    }
+
+
+}
