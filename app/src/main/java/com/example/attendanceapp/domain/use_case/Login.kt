2b985d7b144b6a65ee7ce663c514feb6ab058852
Emery Bashige <bemeryb@gmail.com>
2022-02-03 13:47:51 +0300

    added use case for signing in

---
+++ app/src/main/java/com/example/attendanceapp/domain/use_case/Login.kt
@@ -0,0 +1,23 @@
+package com.example.attendanceapp.domain.use_case
+
+import com.example.attendanceapp.core.utils.OperationStatus
+import com.example.attendanceapp.domain.repository.AttendanceMainRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class Login @Inject constructor(val repo: AttendanceMainRepository){
+
+    suspend operator fun invoke(email: String, password: String): Flow<OperationStatus<String>> {
+
+        if (email.isBlank() || password.isBlank()){
+            return flow {
+                emit(OperationStatus.Error(message = "Email and password can not be empty"))
+            }
+        }
+
+        return repo.signIn(email, password)
+
+    }
+
+}
