490c5df4ff5971d343a8303be6f7d835b8cbf90d
Emery Bashige <bemeryb@gmail.com>
2022-02-03 15:08:47 +0300

    can loggin and collect errors returned

--- app/src/main/java/com/example/attendanceapp/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/example/attendanceapp/presentation/login/LoginFragment.kt
@@ -5,17 +5,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.attendanceapp.R
+import androidx.fragment.app.viewModels
+import com.example.attendanceapp.core.utils.collectLatestLifecycleFlow
+import com.example.attendanceapp.core.utils.ui.showLongSnackBar
+import com.example.attendanceapp.core.utils.ui.stringFromTl
+import com.example.attendanceapp.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+
+    private lateinit var loginFragmentBinding: FragmentLoginBinding
+    private val loginViewModel : LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        loginFragmentBinding =  FragmentLoginBinding.inflate(inflater,container, false)
+        val view = loginFragmentBinding.root
+
+
+        listenToLoginButtonClicked()
+        collectUIEvents()
+
+        return view
+    }
+
+    fun listenToLoginButtonClicked(){
+        loginFragmentBinding.moveToRegisterBt.setOnClickListener{
+            loginViewModel.login(stringFromTl(loginFragmentBinding.emailLoginTl), stringFromTl(loginFragmentBinding.passwordLoginTl))
+        }
+    }
+
+    fun collectUIEvents(){
+        collectLatestLifecycleFlow(loginViewModel.loginEvent){ event ->
+            if(event is LoginViewModel.LoginFragmentUIEvent.showSnackBar){
+                showLongSnackBar(requireView(), event.message)
+            }
+        }
     }
 
 
